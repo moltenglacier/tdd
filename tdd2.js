6874af068fc0db0c229e5f8e62cab95d2e2966be
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-06-11 18:47:41 -0700

    yay

--- tdd2.js
+++ tdd2.js
@@ -5,4 +5,4 @@ var longestString =  function(){
 
 console.assert(longestString("abc","abbb","donkey"), "donkey");
 console.assert(longestString(1,[3,4,5,6,7],"donkey", "hundred"), "hundred");
-console.assert(longestString("1",{one: two, two: three},"money"), "money");
+console.assert(longestString("1",{one: "two", two: "three"},"money"), "money");
